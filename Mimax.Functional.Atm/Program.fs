88763bb3e073b8c429b86908adb164993d1f7616
Mikhail Khodin <khodin@gollard.ru>
2019-10-31 18:10:03 +0300

    atm functional

---
+++ Mimax.Functional.Atm/Program.fs
@@ -0,0 +1,80 @@
+﻿open System
+
+type Account =
+    { Id : Guid
+      Holder : string 
+      Balance : decimal }
+
+type Transaction =
+    { Id : Guid
+      AccountId : Guid
+      Amount : decimal }
+    
+let output (value:string) = Console.WriteLine value
+let input = Console.ReadLine
+
+let persistAccount (account:Account) =
+    let stream = IO.File.AppendText(account.Id.ToString() + ".txt")
+    stream.Write(account.ToString())
+    stream.Dispose()
+
+let persistTransaction (transaction:Transaction) =
+    let stream = IO.File.AppendText(transaction.AccountId.ToString() + ".txt")
+    stream.Write(transaction.ToString())
+    stream.Dispose()
+
+let getAmount (value:string) =
+    let parsed, amount = value |> Decimal.TryParse
+    if(parsed) then amount
+    else failwith "Value is not allowed as amount"
+
+let createAccount =
+    output "Account creation"
+    output "Provide holder name: "
+    let holder = input()
+    let account = { Id = Guid.NewGuid(); Holder = holder; Balance = 0m }
+    output ("Account created: " + account.ToString())
+    account
+
+let apply (account:Account) (transaction:Transaction) = 
+    let balance = account.Balance + transaction.Amount
+    if (balance < 0m) then account
+    else { account with Balance = balance }
+
+let createDeposit (account:Account) = 
+    output "Provide deposit amount: "
+    let amount = input() |> getAmount
+    if (amount < 0m) then failwith "Signed value is not allowed"
+    else { Id = Guid.NewGuid(); AccountId = account.Id; Amount = amount}
+
+let createWithdrawal (account:Account) = 
+    output "Provide withdrawal amount: "
+    let amount = input() |> getAmount
+    if (amount < 0m) then failwith "Signed value is not allowed"
+    else { Id = Guid.NewGuid(); AccountId = account.Id; Amount = -amount}
+
+let createTransaction (account:Account) =
+    output "Select Deposit(D) or Withdrawal(W): "
+    let transaction = input().ToUpper();
+    if (transaction = "D") then createDeposit account
+    elif (transaction = "W") then createWithdrawal account
+    else failwith "Symbol is not allowed"
+
+[<EntryPoint>]
+let main argv =
+    let mutable account = createAccount
+    persistAccount account
+
+    let initial = createDeposit account
+    account <- apply account initial
+    persistTransaction initial
+    output(account.ToString())
+
+    while true do
+        try
+            let transaction = createTransaction account
+            account <- apply account transaction
+            persistTransaction transaction
+            output(account.ToString())
+        with ex -> printf "ERROR: %s" ex.Message
+    0 // return an integer exit code
---
+++ Mimax.Functional/highOrderFunctions.fsx
@@ -0,0 +1,20 @@
+﻿open System
+
+type Customer = { Age : int }
+
+//high order function
+let printCustomerAge writer customer = 
+    let grade =
+        if customer.Age < 12 then "Child"
+        elif customer.Age < 18 then "Teenager"
+        else "Adult"
+    writer grade
+
+//pipeline
+{ Age = 10} |> printCustomerAge Console.WriteLine
+
+//partially implemented function
+let printToConsole = printCustomerAge Console.WriteLine
+printToConsole { Age = 10 }
+printToConsole { Age = 20 }
+printToConsole { Age = 30 }
