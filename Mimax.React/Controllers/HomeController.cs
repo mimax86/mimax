10444c3f908b8c0dc04559cbfdcb2b382f983246
Mikhail Khodin <khodin@gollard.ru>
2020-03-29 10:56:56 +0300

    react tutorial

---
+++ Mimax.React/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Mimax.React.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Mimax.React.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static readonly IList<CommentModel> Data;
+
+        static HomeController()
+        {
+            Data = new List<CommentModel>
+            {
+                new CommentModel
+                {
+                    Id = 1,
+                    Author = "Daniel Lo Nigro",
+                    Text = "Hello ReactJS.NET World!"
+                },
+                new CommentModel
+                {
+                    Id = 2,
+                    Author = "Pete Hunt",
+                    Text = "This is one comment"
+                },
+                new CommentModel
+                {
+                    Id = 3,
+                    Author = "Jordan Walke",
+                    Text = "This is *another* comment"
+                },
+            };
+        }
+
+        [Route("comments")]
+        [ResponseCache(Location = ResponseCacheLocation.None, NoStore = true)]
+        public ActionResult Comments()
+        {
+            return Json(Data);
+        }
+
+        [Route("comments/new")]
+        [HttpPost]
+        public ActionResult AddComment(CommentModel comment)
+        {
+            // Create a fake ID for this comment
+            comment.Id = Data.Count + 1;
+            Data.Add(comment);
+            return Content("Success :)");
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View(Data);
+        }
+    }
+}
